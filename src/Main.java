0a854da9e991f196d9f661d7e1578bc2e5420c2d
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-08-25 20:32:01 +0530

    if else code edit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
-public class Main {
+/******************************************************************************
+
+ Online Java Compiler.
+ Code, Compile, Run and Debug java program online.
+ Write your code in this editor and press "Run" button to execute it.
+
+ *******************************************************************************/
+
+import java.util.*;
+
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int a = 4;
-        int b =8;
-        int c =a+b;
-        System.out.println(c);
+        Scanner sc = new Scanner(System.in);
+        int input;
+        do{
+            int marks = sc.nextInt();
+            if (marks <=100 && marks >=90){
+                System.out.println("This is Good");
+            }
+            else if (marks >=60 && marks <=89){
+                System.out.println("This is also Also Good");
+            }
+            else if (marks >=0 && marks <=59){
+                System.out.println("This is Good as well");
+            }
+            else {
+                System.out.println("invalid");
+            }
+            System.out.println("want to continue ? (yes(1) or no(0))");
+            input = sc.nextInt();
+        }
+        while(input==1);
     }
 }
