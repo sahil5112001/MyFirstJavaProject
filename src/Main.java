5bca125d79f63dd4ef997f54c3711a8c3b47ca4c
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-08-25 20:02:30 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 4;
+        int b =8;
+        int c =a+b;
+        System.out.println(c);
+    }
+}
